4a7e8831f18b5886eeb2deef99473be86ff3f53b
zmzhou-star <zmzhou8@qq.com>
2021-01-31 13:16:49 +0800

    项目初始化
    Signed-off-by: zmzhou-star <zmzhou8@qq.com>

--- src/main/java/com/github/zmzhoustar/webshell/WebShellApplication.java
+++ src/main/java/com/github/zmzhoustar/webshell/WebShellApplication.java
@@ -17,8 +17,6 @@ public class WebShellApplication {
 		// log4j2全局异步日志配置 http://logging.apache.org/log4j/2.x/manual/async.html#AllAsync
 		System.setProperty("Log4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
 		SpringApplication.run(WebShellApplication.class, args);
-		System.out.println("\u9759\u6001\u8D44\u6E90\u7684\u914D\u7F6E\u4F4D\u7F6E\uFF0C\u53EF\u4EE5" +
-				"\u5199\u6210\u4E00\u4E2A\u6570\u7EC4\u914D\u7F6E\u591A\u4E2A\u76EE\u5F55");
 	}
 
 }
